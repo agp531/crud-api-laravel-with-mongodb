9c2370fefbf0e8763dc4a6b316169e90811f4087
Allan <allan.g531@hotmail.com>
2023-12-21 00:30:29 -0300

    created resource routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
+use App\Models\Customer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
@@ -29,3 +31,6 @@ Route::get('/ping', function (Request  $request) {
     }
     return ['msg' => $msg];
     });
+
+
+Route::resource('customers', CustomerController::class)->only(['index','store', 'show', 'update', 'destroy']);
